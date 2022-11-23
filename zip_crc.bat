9a84ab564970be844a412ebd74105845e369b901
leov30 <leo_v30@hotmail.com>
2022-11-22 16:04:28 -0800

    Add files via upload

--- zip_crc.bat
+++ zip_crc.bat
@@ -1,30 +1,47 @@
-
 @echo off
-if not exist "%programfiles%\7-Zip\7z.exe" (
-		echo THIS SCRIPT NEEDS 7zip to be installed&pause&exit
-)
-title Processing files...
-set _7zip="%programfiles%\7-Zip\7z.exe"
+if exist "%programfiles%\7-Zip\7z.exe" set "_7zip=%programfiles%\7-Zip\7z.exe"&goto :next
+if exist "%programfiles(x86)%\7-Zip\7z.exe" set "_7zip=%programfiles(x86)%\7-Zip\7z.exe"&goto :next
+if exist "7z.exe" set "_7zip=7z.exe"&goto :next
+title ERROR
+echo THIS SCRIPT NEEDS 7zip to be installed&pause&exit
 
-set /a _count=0
-(echo File,Path,CRC) >output.csv
+:next
+set /a "_total_lines=0"&set /a "_count_lines=0"
+for %%g in (*.zip *.7z) do set /a _total_lines+=1
+
+if %_total_lines% equ 0 (
+	title ERROR
+	echo THERE ARE NO ZIP./7Z. FILES&pause&exit 
+)
+
+title Processing files...
+set /a _count=0&(echo File,Path,Size,CRC)>output.csv
 
 for %%g in (*.zip *.7z) do (
 	echo %%g
-	%_7zip% l -slt "%%g" >7zip.txt
-	for /f "tokens=2 skip=1 delims==" %%h in ('findstr /b /c:"Path =" /c:"CRC =" 7zip.txt') do call :get_info "%%h" "%%g"
+	for /f "tokens=2 skip=1 delims==" %%h in ('""%_7zip%" l -slt -spd -- "%%g"|findstr /lb /c:"Path =" /c:"Size =" /c:"CRC =""') do (
+		call :get_info "%%h" "%%g"
+	)
+	call :progress
 
 )
-del 7zip.txt
 title FINISHED
 pause&exit
+
+:progress
+set /a "_count_lines+=1"
+set /a "_percent=(%_count_lines%*100)/%_total_lines%"
+title Processing Files: %_count_lines% / %_total_lines% ^( %_percent% %% ^)
+exit /b
 
 :get_info
 set /a _count+=1
 
 if %_count% equ 1 set "_file=%~1"&exit /b
-set /a _count=0
+if %_count% equ 2 set "_size=%~1"&exit /b
+set /a _count=0&set "_crc=%~1"
 
-set "_crc=%~1"
-(echo "%~2","%_file:~1%",%_crc:~1%) >>output.csv
+(echo "%~2","%_file:~1%",%_size:~1%,%_crc:~1%) >>output.csv
+
+set "_file="&set "_size="&set "_crc="
 exit /b
