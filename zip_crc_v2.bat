12bb9007ae29dc58fddb50ef640a28bd109738b2
leov30 <leo_v30@hotmail.com>
2022-11-22 18:44:07 -0800

    Add files via upload

---
+++ zip_crc_v2.bat
@@ -0,0 +1,30 @@
+@echo off
+if exist "%programfiles%\7-Zip\7z.exe" set "_7zip=%programfiles%\7-Zip\7z.exe"&goto :next
+if exist "%programfiles(x86)%\7-Zip\7z.exe" set "_7zip=%programfiles(x86)%\7-Zip\7z.exe"&goto :next
+if exist "7z.exe" set "_7zip=7z.exe"&goto :next
+title ERROR
+echo THIS SCRIPT NEEDS 7ZIP&pause&exit
+:next
+
+title Processing files...
+(echo File,Path,Size,CRC)>output.csv
+"%_7zip%" l -slt -an -ai!*.zip -ai!*.7z >temp.txt
+for /f "tokens=1,2 delims=:=" %%g in ('findstr /lb /c:"Listing archive:" /c:"Path =" /c:"Size =" /c:"CRC =" temp.txt') do call :get_info "%%g" "%%h"
+	
+del temp.txt
+title FINISHED
+pause&exit
+
+:get_info
+if "%~1"=="Listing archive" (
+	set "_file=%~2"&set "_skip=1"
+	exit /b
+)
+if "%_skip%"=="1" set "_skip=0"&exit /b
+if "%~1"=="Path " set "_path=%~2"&exit /b
+if "%~1"=="Size " set "_size=%~2"&exit /b
+if "%~1"=="CRC " set "_crc=%~2"
+
+(echo "%_file:~1%","%_path:~1%",%_size:~1%,%_crc:~1%)>>output.csv
+
+exit /b
